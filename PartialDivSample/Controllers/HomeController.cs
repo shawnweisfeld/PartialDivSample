b1a2afba463cde8b98d20f5b16e1bd3eb82660b3
Shawn Weisfeld <shawn@shawnweisfeld.com>
2013-05-31 12:26:59 -0500

    initial checkin

---
+++ PartialDivSample/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PartialDivSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            ViewBag.Items = Enumerable.Range(1, 10);
+
+            return View();
+        }
+
+        public ActionResult GetNumber(int number)
+        {
+            ViewBag.Number = number;
+            return PartialView();
+        }
+
+    }
+}
---
+++ PartialDivSample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PartialDivSample.MvcApplication" Language="C#" %>
